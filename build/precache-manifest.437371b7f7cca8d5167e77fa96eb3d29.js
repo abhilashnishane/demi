5bfa2f32a20cfbfb9dafda9d10f12a9b1dd3b693
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-09-21 22:50:45 +0530

    built prod code

---
+++ build/precache-manifest.437371b7f7cca8d5167e77fa96eb3d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4aad0a7456367f069276877792e51c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b28a09c1f73650e455a1",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1c80c18e95059c07fe5a",
+    "url": "/static/css/main.03ec14d1.chunk.css"
+  },
+  {
+    "revision": "b28a09c1f73650e455a1",
+    "url": "/static/js/2.013458b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.013458b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c80c18e95059c07fe5a",
+    "url": "/static/js/main.da811df1.chunk.js"
+  },
+  {
+    "revision": "cc0c63def0821f7f21eb",
+    "url": "/static/js/runtime-main.007ca966.js"
+  }
+]);
